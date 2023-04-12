463e7ba5ccbf0c343c72b4f12676b53f9dab6af9
omer-akkoca <omerakkoca1042@gmail.com>
2023-04-12 23:17:48 +0300

    using header compo

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,33 +1,20 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View } from "react-native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ViewMyProfile } from "./ViewMyProfile";
 import { EditMyProfile } from "./EditMyProfile";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { AntDesign, FontAwesome5 } from '@expo/vector-icons'; 
-import { W } from "../../config/constants";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import useAuth from "../../hooks/useAuth"
-
+import { useRoute } from "@react-navigation/native";
+import { Header } from "../../components";
 const Tab = createMaterialTopTabNavigator();
 
 const Profile = () => {
 
-    const { logout } = useAuth()
-    const navigation = useNavigation()
     const { params } = useRoute()
 
     return(
         <SafeAreaView className="flex-1 bg-white">
-            <View className="bg-white flex-row items-center justify-between px-4 pb-1 pt-4">
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <AntDesign name="arrowleft" size={W(5)} color="black" />
-                </TouchableOpacity>
-                <Text className="flex-1 text-center font-bold text-sm">Profile</Text>
-                <TouchableOpacity onPress={() => logout()}>
-                    <FontAwesome5 name="power-off" size={W(5)} color="black" />
-                </TouchableOpacity>
-            </View>
+            <Header title={"Profile"} showLogout/>
             <View className="flex-1 bg-white">
                 <Tab.Navigator
                     screenOptions={{
